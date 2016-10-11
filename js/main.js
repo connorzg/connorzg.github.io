caa340a39cf78793b9379ec8bb2f2c5247bf11e7
Connor Garber <czaneg@gmail.com>
2016-10-10 20:25:42 -0500

    reconfiged shine (using bower)

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   window.addEventListener('mousemove', function(event) {
     shine.light.position.x = event.clientX;
     shine.light.position.y = event.clientY;
-    shine.config.opacity = 0.2;
+    shine.config.opacity = 0.25;
     shine.draw();
   }, false);
 
@@ -12,36 +12,36 @@ $(document).ready(function() {
   var delta = 5;
   var navbarHeight = $('nav').outerHeight();
 
-  $(window).scroll(function(event){
-      didScroll = true;
+  $(window).scroll(function(event) {
+    didScroll = true;
   });
 
   setInterval(function() {
-      if (didScroll) {
-          hasScrolled();
-          didScroll = false;
-      }
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
   }, 250);
 
   function hasScrolled() {
-      var st = $(this).scrollTop();
+    var st = $(this).scrollTop();
 
-      // Make sure they scroll more than delta
-      if(Math.abs(lastScrollTop - st) <= delta)
-          return;
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
 
-      // If they scrolled down and are past the navbar, add class .nav-up.
-      // This is necessary so you never see what is "behind" the navbar.
-      if (st > lastScrollTop && st > navbarHeight){
-          // Scroll Down
-          $('nav').removeClass('nav-down').addClass('nav-up');
-      } else {
-          // Scroll Up
-          if(st + $(window).height() < $(document).height()) {
-              $('nav').removeClass('nav-up').addClass('nav-down');
-          }
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+      // Scroll Down
+      $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+      // Scroll Up
+      if (st + $(window).height() < $(document).height()) {
+        $('nav').removeClass('nav-up').addClass('nav-down');
       }
+    }
 
-      lastScrollTop = st;
+    lastScrollTop = st;
   }
 });
